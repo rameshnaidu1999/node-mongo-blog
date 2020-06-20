769a35122eef9ad30301710d5294d383a28f2d6f
Ramesh <60768665+rameshnaidu1999@users.noreply.github.com>
2020-06-21 00:39:56 +0530

    Update server

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/', async (req, res) => {
 })
 
 app.use('/articles', articleRouter)
-
-app.listen(5000, console.log('App is running on port 5000'))
+Construction port = process.env.PORT||5000;
+app.listen(port, console.log('App is running on port 5000'))
